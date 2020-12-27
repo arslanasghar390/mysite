81956e102437151a381f55f204ca6741ccc59b95
arslanasghar390 <72375768+arslanasghar390@users.noreply.github.com>
2020-12-27 01:32:03 -0800

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("myapp.urls"))
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
